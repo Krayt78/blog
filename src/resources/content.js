317147300e251b2606174bd13ae84d844d49873c
krayt78 <ludovic.domingues96@gmail.com>
2025-07-19 17:04:47 +0200

    test change content

--- src/resources/content.js
+++ src/resources/content.js
@@ -1,16 +1,16 @@
 import { Logo } from "@once-ui-system/core";
 
 const person = {
-  firstName: "Selene",
-  lastName: "Yu",
+  firstName: "Ludovic",
+  lastName: "Domingues",
   get name() {
     return `${this.firstName} ${this.lastName}`;
   },
-  role: "Design Engineer",
+  role: "Blockchain Engineer",
   avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  email: "ludovic.domingues96@gmail.com",
+  location: "Europe/Paris", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "French", "Portuguese", "Spanish"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter = {
